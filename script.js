ab3a5855ad4c7f5cbcc1c19c5bc860aedb3fe8ec
Hasin Eshrak <m.hasin.eshrak@gmail.com>
2024-05-03 15:33:20 +0600

    First & Last Commit

---
+++ script.js
@@ -0,0 +1,72 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-inp");
+let result = document.getElementById("result");
+
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+  console.log(finalURL);
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      result.innerHTML = `
+                <img src="${data[0].flags.svg}" class="flag-img">
+                <h2>${data[0].name.common}</h2>
+                <div class="wrapper">
+                    <div class="data-wrapper">
+                        <h4>Capital:</h4>
+                        <span>${data[0].capital[0]}</span>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="data-wrapper">
+                        <h4>Continent:</h4>
+                        <span>${data[0].continents[0]}</span>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="data-wrapper">
+                        <h4>Population:</h4>
+                        <span>${data[0].population}</span>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="data-wrapper">
+                        <h4>Currency:</h4>
+                        <span>${
+                          data[0].currencies[Object.keys(data[0].currencies)]
+                            .name
+                        } - ${Object.keys(data[0].currencies)[0]}</span>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="data-wrapper">
+                        <h4>Common Languages:</h4>
+                        <span>${Object.values(data[0].languages)
+                          .toString()
+                          .split(",")
+                          .join(", ")}</span>
+                    </div>
+                </div>
+            `;
+
+      let moreDetailsBtn = document.createElement("button");
+      moreDetailsBtn.textContent = "More Details";
+      moreDetailsBtn.classList.add("cta");
+
+      moreDetailsBtn.addEventListener("click", () => {
+        console.log("More Details button clicked!");
+        localStorage.setItem("selectedCountry", JSON.stringify(data[0]));
+        window.location.href = "weather.html";
+      });
+
+      result.appendChild(moreDetailsBtn);
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+    });
+});
